bd9fc48b7b5983895ee22fc7c05f6be0947e8b28
boben195 <boben195@ukr.net>
2025-04-12 20:01:18 +0300

    test

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,23 +1,22 @@
-// async function loader() {
-//   const path = "/api/home-page";
-//   const BASE_URL = "http://localhost:1337";
-//   const url = new URL(path, BASE_URL);
+async function loader() {
+  const path = "/api/home-page";
+  const BASE_URL = "http://localhost:1337";
+  const url = new URL(path, BASE_URL);
 
-//   const response = await fetch(url.href);
-//   const data = await response.json();
-//   console.log(data);
+  const response = await fetch(url.href);
+  const data = await response.json();
+  console.log(data);
   
-//   return { ...data.data };
-// }
+  return { ...data.data };
+}
 
-// export default async function HomeRoute() {
+export default async function HomeRoute() {
 
-//   const data = await loader();
-//   console.log(data);
+  const data = await loader();
+  console.log(data);
   
-//   return <div>
-//     <h1>{data.Title}</h1>
-//     <p>{ data.description}</p>
-//   </div>
-// }
-//ggggggggggggggggggg
+  return <div>
+    <h1>{data.Title}</h1>
+    <p>{ data.description}</p>
+  </div>
+}
